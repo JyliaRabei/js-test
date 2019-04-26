3117f6a44a0d98a07b048b4169e3be66cf6d754d
Jylia Rabei <jyrabey@gmail.com>
2019-04-26 12:55:07 +0000

    Inatial

---
+++ js/index.js
@@ -0,0 +1,5 @@
+//test1//
+function helloWorld() {
+var str = "Hello World!";
+console.log(str)
+}
---
+++ js/last-test.js
@@ -0,0 +1,18 @@
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  var d1="D";
+  var a2="a";
+  var d="d";
+  var par= d1 + a2 + d;
+  return par;
+  }
+function Bee(){
+var b1="B";
+var e2= "e";
+var beer= b1 + e2 + e2;
+return beer;
+}
+function banana(){
+  var ban= b2 + a2 + n2 + a2 + n2 + a2;
+  return ban;
+}
